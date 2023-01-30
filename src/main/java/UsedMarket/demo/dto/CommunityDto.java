c87eaf0bc1c08d684baebf5a8e0dd28fd3089907
hyeok-min <gurals04@gmail.com>
2023-01-30 22:08:36 +0900

    communitydto constructor삭제

--- src/main/java/UsedMarket/demo/dto/CommunityDto.java
+++ src/main/java/UsedMarket/demo/dto/CommunityDto.java
@@ -1,12 +1,11 @@
 package UsedMarket.demo.dto;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-//@NoArgsConstructor
+
 public class CommunityDto {
     private String content;
     private String writer;
