1628074c3eb5cc2d896702ddcd0a61e3ce826e52
hyeok-min <gurals04@gmail.com>
2023-01-30 22:07:56 +0900

    commentdto 수정

--- src/main/java/UsedMarket/demo/dto/CommentDto.java
+++ src/main/java/UsedMarket/demo/dto/CommentDto.java
@@ -2,12 +2,10 @@ package UsedMarket.demo.dto;
 
 import UsedMarket.demo.domain.Board;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-//@NoArgsConstructor
 public class CommentDto {
     private String comment;
     private String writer;
